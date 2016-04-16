43fa5c4ff36c3d86841dafba1d7c660050de3dc5
Kyle Dhillon <kdhillon@princeton.edu>
2016-04-16 06:12:31 -0400

    adds questions.js to server

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var categories = require('./routes/categories');
+var questions = require('./routes/questions');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api/categories', categories);
+app.use('/api/questions', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/questions.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET categories listing. */
+router.get('/', function(req, res, next) {  
+    var articleid = req.query.articleid;
+    
+});
+
+module.exports = router;
