7f6758cd57e8c8e1a17bdef044eb6b6cfd1198fb
Kyle Dhillon <kdhillon@princeton.edu>
2016-04-16 11:50:06 -0400

    replaces mongoose with firebase

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,48 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+var myFirebaseRef = new Firebase("https://blistering-fire-8873.firebaseio.com/");
 
 /* GET categories listing. */
 router.get('/', function(req, res, next) {  
-    res.send({
-      status: true,
-      data: [
-        {
-          id: 1,
-          title: 'Sports',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        },
-        
-        {
-          id: 2,
-          title: 'Animals',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        },
-        
-        {
-          id: 3,
-          title: 'Science',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        },
-        
-        {
-          id: 4,
-          title: 'Art',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        },
-        
-        {
-          id: 5,
-          title: 'Stories',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        },
-        
-        {
-          id: 6,
-          title: 'Music',
-          thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb4sxM2QW5rdHwIAeny0HCHDBwjtgoLIiYJ20fJOxi3FSoEtlhaA'
-        }
-      ]
-    });
+  myFirebaseRef.child("categories").on("value", function(snapshot) {
+      res.send(snapshot.val());
+  })
 });
 
 module.exports = router;
